89c6d4629522d0f55d98366619776275dedde55b
Danilo N da Silva <danilotakaraa@hotmail.com>
2023-08-08 21:37:33 -0300

    nem sei mais o que estou fazendo
    ;-;

--- commands/valtan.js
+++ commands/valtan.js
@@ -1,15 +1,15 @@
 const { SlashCommandBuilder, EmbedBuilder } = require("discord.js")
 
+
 const exampleEmbed = new EmbedBuilder()
 	.setColor("Orange")
 	.setTitle("grupo Valtan")
-	.setThumbnail('https://i.imgur.com/AfFp7pu.png')
 	.addFields(
         { name: '\u200B', value: '\u200B' },
 		{ name: ' Nome:xxx', value: 'classe:xxx', inline: true },
 		{ name: ' Nome:xxx', value: 'classe:xxx', inline: true },
 		{ name: '\u200B', value: '\u200B' },
-		{ name: ' Nome:xxx', value: 'classe:xxx', inline: true },
+		{ name: ' Nome:${nome}', value: 'classe:xxx', inline: true },
 		{ name: ' Nome:xxx', value: 'classe:xxx', inline: true },
         { name: '\u200B', value: '\u200B' },
 		{ name: ' Nome:xxx', value: 'classe:xxx', inline: true },
@@ -20,6 +20,8 @@ const exampleEmbed = new EmbedBuilder()
       
 
 	)
+	.setImage('https://api.boosthive.eu/media/image/1c/be/aed3ac702b10bf3dd14dc133a825.jpeg')
+	
 
 module.exports = {
     data: new SlashCommandBuilder()
